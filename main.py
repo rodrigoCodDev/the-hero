35064852c4ad204b8b7beb43e629efc42236f4be
Rodrigo <rodrigo.oliveira200310@gmail.com>
2025-01-25 10:48:51 -0300

    Adding heroes

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from pick import pick
+from heroes import Hero, HeroData
 
 if __name__ == "__main__":
-    title = "THE HERO GAME - v0.1.0"
+    title = "THE HERO GAME -  v0.1.0"
     options = ['Start new game', 'Load', 'About', 'Exit']
 
     option, index = pick(options, title, indicator='>', default_index=0)
+
+    if (index == 0) :
+        title = "Select your character: "
+        options = []
+        hero_data = ""
+
+        with open('heroes.json', 'r') as file:
+            json_data = file.read()
+            hero_data = HeroData(json_data)
+            options = hero_data.get_hero_names()
+            print(options)
+
+        option, index = pick(options, title, indicator='>', default_index=0)
+        
+    
